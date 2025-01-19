53a409040ec809d351c1a521adad6a08103025c2
Satanshu Mishra <satanshumishra@outlook.com>
2025-01-18 18:10:40 -0700

    Team Page & Bio's Collection Updates
    - Fixed Rory and Stephanie's images being mis-assigned to each other.
    - Updated content collection config (for all collections) to use proper syntax to identify which fields are optional.
    - Added LinkedIn field to Collection Config and all Collections.
    - Added LinkedIn profiles for Anastasia and Satanshu. All other profiles values remain empty.
    - Added the LinkedIn button to the overlay for each person. The button is dynamically assigned the linkedin link and is clickable. To complete, simply add the linkedin profiles for the remaining people.

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,29 +1,30 @@
 import { z, defineCollection } from 'astro:content';
 
 const biosCollection = defineCollection({
-    type: 'data',
-    schema: z.object({
-        firstName: z.string(),
-        lastName: z.string(),
-        role: z.string(),
-        headshot: z.string(),
-        major: z.string(),
+	type: 'data',
+	schema: z.object({
+		firstName: z.string(),
+		lastName: z.string(),
+		role: z.string(),
+		headshot: z.string(),
+		major: z.string(),
 		biography: z.string(),
-		funFact: z.string()
-    })
+		linkedIn: z.string(),
+		funFact: z.string(),
+	}),
 });
 
 const carouselCollection = defineCollection({
-    type: 'data',
-    schema: z.object({
-      title: z.string(),
-      description: z.string(),
-      mainImage: z.string(),
-      secondaryImage: z.string(),
-      alt: z.string(),
-      buttonLink: z.string()
-    })
-  })
+	type: 'data',
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		mainImage: z.string(),
+		secondaryImage: z.string(),
+		alt: z.string(),
+		buttonLink: z.string(),
+	}),
+});
 
 const sponsorsCollection = defineCollection({
 	type: 'data',
@@ -31,21 +32,35 @@ const sponsorsCollection = defineCollection({
 		name: z.string(),
 		logo: z.string(),
 		link: z.string(),
-		tier: z.enum(['bronze', 'silver', 'gold', 'platinum'])	
-	})
-})
+		tier: z.enum(['bronze', 'silver', 'gold', 'platinum']),
+	}),
+});
 
 const faqsCollection = defineCollection({
 	type: 'data',
-	schema: z.object ({
+	schema: z.object({
 		question: z.string(),
-		answer: z.string()
-	})
-})
+		answer: z.string(),
+	}),
+});
+
+const eventsCollection = defineCollection({
+	type: 'data',
+	schema: z.object({
+		title: z.string(),
+		startTime: z.string(),
+		endTime: z.string(),
+		speaker: z.optional(z.string()),
+		location: z.optional(z.string()),
+		color: z.optional(z.string()),
+		description: z.optional(z.string()),
+	}),
+});
 
 export const collections = {
-    'bios': biosCollection,
+	bios: biosCollection,
 	carousel: carouselCollection,
-	'sponsors': sponsorsCollection,
-	'faqs': faqsCollection,
+	sponsors: sponsorsCollection,
+	faqs: faqsCollection,
+	events: eventsCollection,
 };
