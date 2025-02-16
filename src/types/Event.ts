2899a7ad804dda9678a8a9fb5aa4bf12b1613a5f
Satanshu Mishra <satanshumishra@outlook.com>
2025-02-16 11:19:49 -0700

    Type Security, Schedule Bug Fixes & Overlay Changes
    - Added comprehensive type security to schedule page for easier troubleshooting in the future.
    - Fixed several bugs on the schedule page. These include,
    	- Multiple Location and Map(s) showing up inside the Pop-up window.
    	- The Pop-up being triggered once the AddToCalendar pop-up was closed.
    	- Location and Building information not being rendered correctly on Pop-up.
    	- Clicking on breaks triggering a pop-up -> Now clicking on breaks does nothing.
    	- Category classes not having color.
    - Added Type security to CategoryPill for improved clarity on the props being pased in.
    - Added a Close Button to every Overlay (along side existing functionality of cicking outside a pop-up) for improved accessibility.

---
+++ src/types/Event.ts
@@ -0,0 +1,60 @@
+const UBCOBuildings = {
+	ADM: 'Administration Building',
+	ART: 'Arts Building',
+	ASC: 'Arts and Sciences Centre',
+	COM: 'Commons Building',
+	EME: 'Engineering, Management and Education Building',
+	FIP: 'Fipke Centre for Innovative Research',
+	GYM: 'Gymnasium',
+	LIB: 'Library',
+	SCI: 'Science Building',
+	RHS: 'Reichwald Health Sciences Centre',
+	UNC: 'University Centre',
+	SSC: 'Student Services Centre',
+	CCS: 'Creative and Critical Studies Building',
+	ARTS: 'Arts and Sciences Building',
+	MH4: 'Mountain Home 4 (Arts House)',
+	NIC: 'Nicola Residence',
+	KLO: 'Kalamalka Residence',
+	MON: 'Monashee Residence',
+	PUC: 'Purcell Residence',
+	SIM: 'Similkameen Residence',
+	SKA: 'Skeena Residence',
+	VAN: 'Valhalla Residence',
+	CAS: 'Cassiar Residence',
+	CSC: 'Cascades Residence',
+	NHP: 'Nechako Residence',
+} as const;
+
+type BuildingCode = keyof typeof UBCOBuildings;
+
+type OnCampusLocation = {
+  onCampus: true;
+  street: string;
+  postalCode: string;
+  buildingCode: BuildingCode;
+  room: string;
+}
+
+type OffCampusLocation = {
+  onCampus: false;
+  street: string;
+  postalCode: string;
+  map: string;
+}
+
+export default interface Event {
+  category: "Keynote" | "Bureaucracy" | "Plenary" | "Exploration" | "Leisure" | "Break" | "Presentations" | "Workshop";
+  title: string;
+  startTime: string;
+  endTime: string;
+  speaker?: {
+    name: string;
+    website: string;
+  };
+  location: OnCampusLocation | OffCampusLocation;
+  calendarDescription: string;
+  websiteDescription: string;
+  isColored: boolean;
+}
+
